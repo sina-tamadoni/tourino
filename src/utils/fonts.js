88704f2e486a0ac393cb29392143abb20432386c
sina <ss.tamadoni@gmail.com>
2024-12-08 11:24:28 +0330

    Initial commit from Create Next App

---
+++ src/utils/fonts.js
@@ -0,0 +1,56 @@
+import localFont from "next/font/local";
+
+export const yekan = localFont({
+  src: [
+    {
+      path: "../app/fonts/YekanBakh-Light.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/YekanBakh-Regular.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/YekanBakh-Bold.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/YekanBakh-Heavy.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/YekanBakh-Fat.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
+
+export const vazir = localFont({
+  src: [
+    {
+      path: "../app/fonts/Vazirmatn-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazirmatn-SemiBold.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazirmatn-Medium.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../app/fonts/Vazirmatn-Bold.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+});
